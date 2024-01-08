d13c0e5f580741bc4b067cf36a58afd7e095f355
Kojima132 <kurumitokisaki1322004@gmail.com>
2024-01-08 15:38:09 +0700

    Update

---
+++ jsforB2-LAB-5-B5.js
@@ -0,0 +1,10 @@
+
+function ham(x){
+    if(x%2==0){
+        document.write("Giá trị bạn vừa nhập là:"+x+"\n");
+        document.writeln("Số chẵn");
+    }else{
+        document.write("Giá trị bạn vừa nhập là:"+x+"\n");
+        document.writeln("Số lẻ");
+    }
+}
