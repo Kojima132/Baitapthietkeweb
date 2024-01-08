992067fc6f666a942f3b75df81c00afa73d59950
Kojima132 <kurumitokisaki1322004@gmail.com>
2024-01-08 19:33:45 +0700

    Update

--- jsforB2-LAB-5-B5.js
+++ jsforB2-LAB-5-B5.js
@@ -1,10 +1,10 @@
 
 function ham(x){
     if(x%2==0){
-        document.write("Giá trị bạn vừa nhập là:"+x+"\n");
-        document.writeln("Số chẵn");
+        document.write("Giá trị bạn vừa nhập là: " + x + "<br>");
+        document.write("Số chẵn");
     }else{
-        document.write("Giá trị bạn vừa nhập là:"+x+"\n");
-        document.writeln("Số lẻ");
+        document.write("Giá trị bạn vừa nhập là: " + x + "<br>");
+        document.write("Số lẻ");
     }
 }
